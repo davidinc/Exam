d76a9a7ab6df696d406f3980f8529ab76b7b5a8b
Dawit Ayalew <davidintsum@gmail.com>
2023-12-11 20:33:12 -0500

    Generic Final Exam
    THis folder contain the Generic solution

---
+++ Generic/Generic/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class VTypeCol<T> where T : struct // Constraint for value types
+{
+    private List<T> col;
+
+    public VTypeCol()
+    {
+        col = new List<T>();
+    }
+
+    public void AddItem(T item)
+    {
+        col.Add(item);
+    }
+
+    public T GetItem(int index)
+    {
+        if (index >= 0 && index < col.Count)
+        {
+            return col[index];
+        }
+        else
+        {
+            throw new IndexOutOfRangeException("Index is out of range");
+        }
+    }
+
+    public List<T> DescendingOrderSorted()
+    {
+        List<T> sortCol = col.OrderByDescending(x => x).ToList();
+        return sortCol;
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        VTypeCol<int> Collect = new VTypeCol<int>();
+        Collect.AddItem(2);
+        Collect.AddItem(6);
+        Collect.AddItem(4);
+
+        Console.WriteLine("Item at index 1: " + Collect.GetItem(1));
+
+        List<int> des = Collect.DescendingOrderSorted();
+        Console.WriteLine("Des Sorted Collection:");
+        foreach (int item in des)
+        {
+            Console.WriteLine(item);
+        }
+    }
+}
